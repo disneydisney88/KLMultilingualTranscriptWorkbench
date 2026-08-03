6c56ba1d456a8de712463cfb8271230d827c1e0e
disneydisney88 <131468402+disneydisney88@users.noreply.github.com>
2026-08-03 02:52:43 +0100

    Add version metadata

---
+++ version.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+APP_NAME = "KL 憭?閮??蝔踹極雿"
+APP_NAME_EN = "KL Multilingual Transcript Workbench"
+APP_VERSION = "2026.08.01.5"
+APP_RELEASE_DATE = "2026-08-01"
