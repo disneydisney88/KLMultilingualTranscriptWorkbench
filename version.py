fd344dac542686fa1eb1a7632706870525e5c08a
disneydisney88 <131468402+disneydisney88@users.noreply.github.com>
2026-08-05 07:00:22 +0100

    Update version to 2026.08.05.1

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-APP_NAME = "KL 憭?閮??蝔踹極雿"
+APP_NAME = "KL 多語言逐字稿工作台"
 APP_NAME_EN = "KL Multilingual Transcript Workbench"
-APP_VERSION = "2026.08.01.5"
-APP_RELEASE_DATE = "2026-08-01"
+APP_VERSION = "2026.08.05.1"
+APP_RELEASE_DATE = "2026-08-05"
